13e58d1fbb4d5ef5e43a18446940946ec3d58282
Rajshekhar98 <singhraj9130@gmail.com>
2026-02-25 17:07:31 +0530

    Update run_tests.groovy

--- vars/run_tests.groovy
+++ vars/run_tests.groovy
@@ -2,6 +2,7 @@ def call() {
     sh '''
     echo "Running unit tests..."
     if [ -f package.json ]; then
+        npm install
         CI=true npm test -- --watchAll=false --passWithNoTests || echo "Tests failed but continuing"
     else
         echo "No package.json found, skipping tests"
